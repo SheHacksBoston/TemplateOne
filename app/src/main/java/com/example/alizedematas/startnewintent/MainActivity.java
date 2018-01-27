2c9ce24a59d9819abe7fdfc0abd2c2b92811a4a3
AlizeDeMatas <Alize@github.io>
2018-01-27 13:36:10 -0500

    Initialize File

---
+++ app/src/main/java/com/example/alizedematas/startnewintent/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.alizedematas.startnewintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button)findViewById(R.id.start);
+        text = (TextView)findViewById(R.id.old);
+
+start.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent neo = new Intent(MainActivity.this,NewIntent.class);
+        startActivity(neo);
+
+    }
+});
+
+
+    }
+}
